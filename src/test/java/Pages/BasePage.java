b015818256b62e3226e3db69f8837a0aad614488
Raychinec Vasil <59335374+RaychinecV@users.noreply.github.com>
2020-02-02 00:13:12 +0200

    Added allure

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -3,16 +3,12 @@ package Pages;
 import TestCases.BaseTest;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage extends BaseTest {
-    public static WebDriver getDriver() {
-        return driver;
-    }
 
-    WebDriverWait wait = new WebDriverWait(getDriver(), 10);
+    WebDriverWait wait = new WebDriverWait(driver, 10);
 
     static final Logger log = Logger.getLogger(BasePage.class);
 
